4cfe0e7a0810ee383656995cf7f0090b195b032c
Infant Ajay Venus <infantcsajay@outlook.com>
2024-08-31 23:50:37 +0530

    [util] feat: add getdefinitionslist function

---
+++ utils/getDefinitionsList.ts
@@ -0,0 +1,11 @@
+import { readdirSync } from "fs";
+import { join } from "path";
+
+const definitionsPath = join(process.cwd(), 'public', 'definitions');
+
+export function getDefinitionsList() {
+
+    return readdirSync(definitionsPath, { withFileTypes: true })
+        .filter((fileEntry) => fileEntry.isFile())
+        .map((fileItem) => fileItem.name);
+}
