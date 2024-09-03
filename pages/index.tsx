55888c1d1a4706e7c5a21acfadfbb3d7f6c428a0
Infant Ajay Venus <infantcsajay@outlook.com>
2024-09-03 11:00:46 +0530

    [index] patch: copy write changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,7 +27,7 @@ export default function Home({ definitionsList }: HomeProps) {
       <header className="border-b-2 py-8">
         <h3 className="text-3xl mb-4">OpenAPI Definitions</h3>
         <p>Below is a list of OpenAPI definitions of third party APIs. These definitions were created by referring to the official API documentation.</p>
-        <p><strong>Disclaimer:</strong><em> These definitions were written as part of a learning activity, so it is prone to have errors and is not intended to sync with the changes in the documentation. </em></p>
+        <p><strong>Disclaimer:</strong><em> These definitions were written as part of a learning activity, so it is prone to have errors and is not intended to be in sync with the changes in the respective documentation. </em></p>
       </header>
       <section className="p-4">
         <ul>
