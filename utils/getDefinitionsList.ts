cc1b549a3d094e445d7af750971eea97d111e1b6
Infant Ajay Venus <infantcsajay@outlook.com>
2024-09-04 23:34:14 +0530

    [util] fix: modify filenames in getDefinitionsList

--- utils/getDefinitionsList.ts
+++ utils/getDefinitionsList.ts
@@ -7,5 +7,5 @@ export function getDefinitionsList() {
 
     return readdirSync(definitionsPath, { withFileTypes: true })
         .filter((fileEntry) => fileEntry.isFile())
-        .map((fileItem) => fileItem.name);
+        .map((fileItem) => fileItem.name.split('.').slice(0, -1).join('.'));
 }
